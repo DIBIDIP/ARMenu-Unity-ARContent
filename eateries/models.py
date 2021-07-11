4eb4528dd4ce8dbd8dd13ac9ef851b0cc73727a2
kimkyury <mandarining0918@gmail.com>
2021-07-11 23:26:07 +0900

    Feat: brandName 패널 생성

--- eateries/models.py
+++ eateries/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from core import models as core_models
 
 # Create your models here.
+
+# 부모메소드
+class AbstractItem(core_models.TimeStampedModel):
+    
+    """ Abstract Item """
+
+    name = models.CharField(max_length=20)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+# 브랜드이름
+class brandName(AbstractItem):
+    """  Brand Model Definition """
+
+    class Meta:
+        verbose_name = "Brand Name"
