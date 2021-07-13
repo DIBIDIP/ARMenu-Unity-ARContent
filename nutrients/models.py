9035c69c313f0583c47e8cc4d5ae704c10b46224
kimkyury <mandarining0918@gmail.com>
2021-07-13 22:12:38 +0900

    Feat: 탄단지 모델 생성

--- nutrients/models.py
+++ nutrients/models.py
@@ -1,3 +1,37 @@
 from django.db import models
-
+from core import models as core_models
 # Create your models here.
+
+
+class AbstractItem(core_models.TimeStampedModel):
+
+    """ Abstract Item """
+
+    name = models.CharField(max_length=80)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.name
+
+
+class Carbohydrate(AbstractItem):
+    """  Carbohydrate Model Definition """
+    
+    class Meta:
+        verbose_name_plural = "Carbohydrate"
+
+
+class Protein(AbstractItem):
+    """  Protein Model Definition """
+    
+    class Meta:
+        verbose_name_plural = "Protein"
+
+
+class fat(AbstractItem):
+    """  fat Model Definition """
+    
+    class Meta:
+        verbose_name_plural = "fat"
