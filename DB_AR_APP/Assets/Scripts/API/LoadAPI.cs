3785381ac09c0b2ee8295964b98afcbc886ebfe6
Hooooooon <shonn2323@gmail.com>
2021-08-23 20:55:41 +0900

    Feat: 로딩화면 개발
    - 텍스트 바운스 애니메이션 추가

--- DB_AR_APP/Assets/Scripts/API/LoadAPI.cs
+++ DB_AR_APP/Assets/Scripts/API/LoadAPI.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
 
+
 public class LoadAPI : MonoBehaviour
 {
 
@@ -13,25 +14,37 @@ public class LoadAPI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        StartCoroutine(LoadData());
+        if(Application.internetReachability == NetworkReachability.NotReachable){
+            Debug.LogError("인터넷 연결 안됨.");
+        }else{
+            StartCoroutine(LoadData());
+        }
     }
+    
+    // json 처리
+    void JsonParsing(){
+        string json = JsonUtility.ToJson(resultJson);
+        Debug.Log(json);
+    }   
 
     // HTTP 통신을 통해 Json 받아오기
     IEnumerator LoadData() {
-        string getDataURL = "";
+        string getDataURL = "localhost:8080/hello-api?name=유니티안녕";
         // HTTP Get 요청 보내기
-        using(UnityWebRequest www = UnityWebRequest.Get(getDataURL)) 
+        using(UnityWebRequest request = UnityWebRequest.Get(getDataURL)) 
         {
-            yield return www.Send();
-            if (www.isNetworkError || www.isHttpError){ // 불러오기 실패 
-                Debug.Log(www.error);
-            }else{
-                if (www.isDone){ // 통신 성공
+            yield return request.SendWebRequest();    // 요청 보내기
+
+            if (request.isNetworkError || request.isHttpError){ // 불러오기 실패 
+                Debug.Log(request.error);
+            }else{  
+                IsSuccesLoadData = true;
+                resultJson = System.Text.Encoding.UTF8.GetString(request.downloadHandler.data);                    
+                Debug.Log(resultJson);
+
+                if (request.responseCode != 200 ){ // 통신 성공
                     // json으로 받기
-                    IsSuccesLoadData = true;
-                    resultJson = System.Text.Encoding.UTF8.GetString(www.downloadHandler.data);
-                    
-                    Debug.Log(resultJson);
+                    Debug.LogError("통신 실패");
                 }
             }
         }
