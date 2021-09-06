f5cde67ef0e4d1c2a72e1b249941b7431571511c
OHYUNBEOM <66807406+OHYUNBEOM@users.noreply.github.com>
2021-09-06 21:33:13 +0900

    Feat : Final Modifying

--- client/src/components/views/FavoritePage/FavoritePage.js
+++ client/src/components/views/FavoritePage/FavoritePage.js
@@ -7,30 +7,30 @@ import { IMAGE_BASE_URL } from '../../Config';
 function FavoritePage() {
     const [Favorites, setFavorites] = useState([])
     useEffect(() => {
-        fetchFavoredMovie()
+        fetchFavoredMenu()
     }, [])
-    const fetchFavoredMovie = () => {
-        Axios.post('/api/favorite/getFavoredMovie', { userFrom: localStorage.getItem('userId') })//favoried 한 UserId를 가져와야함
+    const fetchFavoredMenu = () => {
+        Axios.post('/api/favorite/getFavoredMenu', { userFrom: localStorage.getItem('userId') })//favoried 한 UserId를 가져와야함
             .then(response => {
                 if (response.data.success) {
                     setFavorites(response.data.favorites)
                 } else {
-                    alert('영화 정보를 가져오는데 실패 했습니다.')
+                    alert('메뉴 정보를 가져오는데 실패 했습니다.')
                 }
             })
     }
 
-    const onClickDelete = (movieId, userFrom) => {
+    const onClickDelete = (menuId, userFrom) => {
 
         const variables = {
-            movieId,
+            menuId,
             userFrom
         }
 
         Axios.post('/api/favorite/removeFromFavorite', variables)
             .then(response => {
                 if (response.data.success) {
-                    fetchFavoredMovie()
+                    fetchFavoredMenu()
                 } else {
                     alert("리스트에서 지우는데 실패했습니다.")
                 }
@@ -41,8 +41,8 @@ function FavoritePage() {
     const renderCards = Favorites.map((favorite, index) => {
         const content = (
             <div>
-                {favorite.moviePost ?
-                    <img src={`${IMAGE_BASE_URL}w500${favorite.moviePost}`} /> : "no image"}
+                {favorite.menuPost ?
+                    <img src={`${IMAGE_BASE_URL}w500${favorite.menuPost}`} /> : "no image"}
                     {/* Popover 에 띄워줄 이미지를 content 에 넣어준다 */}
             </div>
         )
@@ -50,12 +50,12 @@ function FavoritePage() {
 
         return <tr key={index}>
 
-            <Popover content={content} title={`${favorite.movieTitle}`} >
-                <td>{favorite.movieTitle}</td>
+            <Popover content={content} title={`${favorite.menuTitle}`} >
+                <td>{favorite.menuTitle}</td>
             </Popover> {/* Popover 로 감싸주게되면 커서를 올렸을때, content 에 넣어둔 사진과함께 title 이 보여짐 */}
 
-            <td>{favorite.movieRunTime} mins</td>
-            <td><button onClick={() => onClickDelete(favorite.movieId, favorite.userFrom)}>Remove</button></td>
+            <td>{favorite.menuRunTime} 분(min)</td>
+            <td><button onClick={() => onClickDelete(favorite.menuId, favorite.userFrom)}>Remove</button></td>
 
         </tr>
     })
@@ -64,15 +64,15 @@ function FavoritePage() {
 
     return (
         <div style={{ width: '85%', margin: '3rem auto' }}>
-            <h2> Favorite Movies </h2>
+            <h2> 즐겨찾기 메뉴 </h2>
             <hr />
 
             <table>
                 <thead>
                     <tr>
-                        <th>Menu Name(메뉴이름)</th>
-                        <th>Cooking Time(제조 시간)</th>
-                        <td>Remove from favorites</td>
+                        <th>[메뉴 이름]</th>
+                        <th>[제조 시간]</th>
+                        <td>[즐겨찾기 삭제]</td>
                     </tr>
                 </thead>
                 <tbody>
