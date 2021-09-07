47d16768ba6b0a98715c296922f15907632f9a14
kimkyury <mandarining0918@gmail.com>
2021-09-07 21:09:11 +0900

    Finished

--- README.md
+++ README.md
@@ -1,13 +1,8 @@
-<img src = "https://user-images.githubusercontent.com/71677810/127866209-aaed683e-b10a-4772-8195-55c15e3fb6c9.png" width="30%" height="30%">
+# ARMenu-Unity-ARContent
 
-# DIBIDIP의 AR Dish
+AR을 이용하여 가상 메뉴판을 구현합니다.
 
-AR기능이 구현되는 메뉴판을 제작
+# 실행방법
 
-## AR Dish 개발 환경
-
-NodsJS, MongoDB, Express, VScode, React 등
-
-## 해당 레포지토리는
-
-- API 제작 연습을 진행합니다.
+- npm i
+- npm run backend
--- server/index.js
+++ server/index.js
@@ -1,40 +1,52 @@
-require("dotenv").config();
+require("dotenv").config(); // .env 파일 적용
 const express = require("express");
-const cors = require("cors");
+const cors = require("cors"); // B-F 서버 차이(CORS)해결을 위한 미들웨어
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
+const fs = require("fs-extra");
 
 const app = express();
 const path = require("path");
 const config = require("./config/key");
-const port = process.env.PORT || 3001;
-
 const mongoose = require("mongoose");
+const { PassThrough } = require("stream");
+const os = require("os");
+
+// const mongoose = require("mongoose");
+// mongoose
+//   .connect(config.mongoURI, { useNewUrlParser: true })
+//   .then(() => console.log("DB connected"))
+//   .catch(err => console.error(err));
+
+const port = process.env.PORT;
+
 mongoose.Promise = global.Promise;
 const connect = mongoose
   .connect(config.mongoURI)
   .then(() => console.log("✔️ MongoDB 연결했어요 "))
   .catch((err) => console.log(err));
 
-/* 패키지 적용 */
-// 서버 차이 해결
 app.use(cors());
-// 가공된 형태로 보내고-받고-접근할 수 있도록 bodyParser 적용 => 디폴트값(Undefind)오류 해결
+
+//to not get any deprecation warning or error
+//support parsing of application/x-www-form-urlencoded post data
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
+//to get json data
+// support parsing of application/json type post data
 app.use(bodyParser.json());
-//요청된 쿠키를 쉽게 추출하도록 함
 app.use(cookieParser());
 
-/* 기존 api */
 app.use("/api/users", require("./routes/users"));
 app.use("/api/favorite", require("./routes/favorite"));
 app.use("/api/comment", require("./routes/comment"));
 app.use("/api/like", require("./routes/like"));
 
-/* 새로 만든 MENU api */
+/* api 생성 */
 app.use("/api/menus", require("./api/menuRouter")); // api폴더 > menusRouter.js 내용 참고
 
+//use this to show the image you have in node js server to client (react js)
+//https://stackoverflow.com/questions/48914987/send-image-path-from-node-js-express-server-to-react-client
 app.use("/uploads", express.static("uploads"));
 
 // Serve static assets if in production
@@ -48,6 +60,9 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+const port = process.env.PORT || 5000;
+
+/* 서버 연결 */
 app.listen(port, () => {
   console.log(
     `✔️ http://localhost:` + port + `, ${port}port에서 대기하고 있습니다.`
